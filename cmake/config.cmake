9a71dc20ddba0e81c05658ecc1aa3d7e0f161366
cyjseagull <1547158861@qq.com>
2021-05-06 11:30:51 +0800

    Initialize the bcos-txpool project (#1)

---
+++ cmake/config.cmake
@@ -0,0 +1,5 @@
+hunter_config(bcos-framework
+VERSION 3.0.0-local
+URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/8cfedafa6eaa1be04a058590b465d7cdd8208adb.tar.gz"
+SHA1 f9074836ae8394fc9e1964b9160c52ad536b4b4c
+)
